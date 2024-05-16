f8ddf2be1db1223544f6cee6b7f0a42d5489e7b7
bo-zzung <135571673+bo-zzung@users.noreply.github.com>
2024-05-16 23:43:35 +0900

    Update CIFAR-10.py

--- Deep Learning/CIFAR-10.py
+++ Deep Learning/CIFAR-10.py
@@ -2,8 +2,9 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.keras.datasets as ds
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, LeakyReLU
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 import matplotlib.pyplot as plt
 
 
@@ -12,27 +13,31 @@ x_train = x_train.reshape(50000, 3072)
 x_test = x_test.reshape(10000, 3072)
 x_train = x_train.astype(np.float32) / 255.0  
 x_test = x_test.astype(np.float32) / 255.0
-y_train = tf.keras.utils.to_categorical(y_train, 10)  
+y_train = tf.keras.utils.to_categorical(y_train, 10) 
 y_test = tf.keras.utils.to_categorical(y_test, 10)
 
-
 dmlp = Sequential()
-dmlp.add(Dense(units=1024, activation='relu', input_shape=(3072,)))
-dmlp.add(BatchNormalization())
-dmlp.add(Dropout(0.5))
-dmlp.add(Dense(units=512, activation='relu'))
+dmlp.add(Dense(units=1024, input_shape=(3072,)))
+dmlp.add(LeakyReLU(alpha=0.1))
 dmlp.add(BatchNormalization())
 dmlp.add(Dropout(0.4))
-dmlp.add(Dense(units=512, activation='relu'))
+dmlp.add(Dense(units=512))
+dmlp.add(LeakyReLU(alpha=0.1))
 dmlp.add(BatchNormalization())
 dmlp.add(Dropout(0.3))
+dmlp.add(Dense(units=512))
+dmlp.add(LeakyReLU(alpha=0.1))
+dmlp.add(BatchNormalization())
+dmlp.add(Dropout(0.2))
 dmlp.add(Dense(units=10, activation='softmax'))
 
 
-dmlp.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.0003), metrics=['accuracy'])
+lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=1e-6)
+
+dmlp.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.0005), metrics=['accuracy'])
 
 
-hist = dmlp.fit(x_train, y_train, batch_size=128, epochs=50, validation_data=(x_test, y_test), verbose=2)
+hist = dmlp.fit(x_train, y_train, batch_size=128, epochs=100, validation_data=(x_test, y_test), verbose=2, callbacks=[lr_scheduler])
 
 
 accuracy = dmlp.evaluate(x_test, y_test, verbose=0)[1] * 100
@@ -57,4 +62,3 @@ plt.ylabel('Loss')
 plt.legend(['Training', 'Validation'])
 plt.grid(True)
 plt.show()
-
