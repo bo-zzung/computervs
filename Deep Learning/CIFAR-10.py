3124bd34212be64f9c25a2859a809a954ed1ec9e
bo-zzung <135571673+bo-zzung@users.noreply.github.com>
2024-05-16 23:30:43 +0900

    Create CIFAR-10.py

---
+++ Deep Learning/CIFAR-10.py
@@ -0,0 +1,60 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.datasets as ds
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras.optimizers import Adam
+import matplotlib.pyplot as plt
+
+
+(x_train, y_train), (x_test, y_test) = ds.cifar10.load_data()
+x_train = x_train.reshape(50000, 3072)  
+x_test = x_test.reshape(10000, 3072)
+x_train = x_train.astype(np.float32) / 255.0  
+x_test = x_test.astype(np.float32) / 255.0
+y_train = tf.keras.utils.to_categorical(y_train, 10)  
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+
+dmlp = Sequential()
+dmlp.add(Dense(units=1024, activation='relu', input_shape=(3072,)))
+dmlp.add(BatchNormalization())
+dmlp.add(Dropout(0.5))
+dmlp.add(Dense(units=512, activation='relu'))
+dmlp.add(BatchNormalization())
+dmlp.add(Dropout(0.4))
+dmlp.add(Dense(units=512, activation='relu'))
+dmlp.add(BatchNormalization())
+dmlp.add(Dropout(0.3))
+dmlp.add(Dense(units=10, activation='softmax'))
+
+
+dmlp.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.0003), metrics=['accuracy'])
+
+
+hist = dmlp.fit(x_train, y_train, batch_size=128, epochs=50, validation_data=(x_test, y_test), verbose=2)
+
+
+accuracy = dmlp.evaluate(x_test, y_test, verbose=0)[1] * 100
+print('정확도:', accuracy)
+
+
+plt.plot(hist.history['accuracy'], 'r--')
+plt.plot(hist.history['val_accuracy'], 'b')
+plt.title('Accuracy graph')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(['Training', 'Validation'])
+plt.grid(True)
+plt.show()
+
+
+plt.plot(hist.history['loss'], 'r--')
+plt.plot(hist.history['val_loss'], 'b')
+plt.title('Loss graph')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend(['Training', 'Validation'])
+plt.grid(True)
+plt.show()
+
